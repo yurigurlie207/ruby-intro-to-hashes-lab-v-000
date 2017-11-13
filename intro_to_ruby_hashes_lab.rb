05c565f1856e87143c623ab134dc60e10ea3d977
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-13 22:47:06 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
 															  	{ one_piece_owned: 25,
 																		two_pieces_owned: 50,
 																		three_pieces_owned: 100,
-																		four_piece_owned: 200
+																		four_pieces_owned: 200
 																	}
 															}
 						}
