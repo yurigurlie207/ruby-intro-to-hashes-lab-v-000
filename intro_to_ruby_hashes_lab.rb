dab18dc8f14bb38e86d572c96019e2c2402aebb4
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-13 22:53:41 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,9 +41,17 @@ def monopoly_with_fourth_tier
 	monopoly = { :railroads => { :pieces => 4,
 															 :names =>
 															 {
-																 reading_railroad: {},
-																 pennsylvania_railroad: {},
-																 b_and_o_railroad: {},
+																 reading_railroad: 
+																 {
+																	 "mortgage_value" => "$100"
+																 },
+																 pennsylvania_railroad: 
+																 {
+																	  "mortgage_value" => "$200"
+																 },
+																 b_and_o_railroad: {
+																	 "mortgage_value" => "$400"
+																 },
 																 shortline: {}
 															 },
 															 :rent_in_dollars =>
