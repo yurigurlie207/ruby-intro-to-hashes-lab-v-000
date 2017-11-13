fc58535c91ed6017d55332082595650762b4bdc1
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-13 22:54:15 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,11 +49,11 @@ def monopoly_with_fourth_tier
 																 {
 																	  "mortgage_value" => "$200"
 																 },
-																 b_and_o_railroad: 
+																 b_and_o_railroad:
 																 {
 																	  "mortgage_value" => "$400"
 																 },
-																 shortline: 
+																 shortline:
 																 {
 																	 "mortgage_value" => "$800"
 																 }
