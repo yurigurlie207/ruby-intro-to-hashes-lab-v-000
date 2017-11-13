29d20beb1e5a88b8901ea8781cf50bd8058a053c
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-13 22:43:58 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,10 +21,11 @@ def monopoly_with_third_tier
 	monopoly = { :railroads => { :pieces => 4,
 														   :names => {},
 															 :rent_in_dollars =>
-															  	{ one_piece_owned: 25, 
-																		two_piece_owned: 50, 
+															  	{ one_piece_owned: 25,
+																		two_piece_owned: 50,
 																		three_piece_owned: 100,
-																		four_piece_owned: 200}
+																		four_piece_owned: 200
+																	}
 															}
 						}
 end
